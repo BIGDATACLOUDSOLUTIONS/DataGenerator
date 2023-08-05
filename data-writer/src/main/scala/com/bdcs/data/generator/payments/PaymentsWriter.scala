fae642b4cd2ae6d307363380417aced423024d03
RAJESH <bigdatacloudsolution@gmail.com>
2023-08-05 21:29:38 +0530

    Fixed null customerId and StoreId in Payments

--- data-writer/src/main/scala/com/bdcs/data/generator/payments/PaymentsWriter.scala
+++ data-writer/src/main/scala/com/bdcs/data/generator/payments/PaymentsWriter.scala
@@ -2,25 +2,20 @@ package com.bdcs.data.generator.payments
 
 import org.apache.kafka.clients.producer.{KafkaProducer, Producer, ProducerConfig, ProducerRecord}
 import io.confluent.kafka.serializers.KafkaAvroSerializer
-
 import com.bdcs.data.generator.lib.customer.Customer
 import com.bdcs.data.generator.lib.product.Product
 import com.bdcs.data.generator.lib.store.Store
 import com.bdcs.data.generator.lib.payment.Payment
-
 import com.bdcs.data.generator.customers.CustomersWriter
 import com.bdcs.data.generator.products.ProductsWriter
 import com.bdcs.data.generator.stores.StoresWriter
-
 import com.bdcs.data.generator.common.AppConfig._
-
 import com.bdcs.data.generator.avro.payment.{InvoiceAvro, PaymentAvro}
 import com.bdcs.data.generator.json.payment.{InvoiceJson, PaymentJson}
-
 import com.bdcs.data.generator.invoices.serializer.InvoiceJsonSerializer
 import com.bdcs.data.generator.payments.serializer.PaymentJsonSerializer
 import com.bdcs.data.generator.common.AsynchronousProducerCallback
-
+import com.bdcs.data.generator.invoices.{InvoiceAvroPayload, InvoiceJsonPayload}
 import com.bdcs.data.generator.payments.PaymentJsonPayload.{getPaymentJsonPayload, getPaymentMasterJsonPayload}
 import com.bdcs.data.generator.payments.PaymentAvroPayload.{getPaymentAvroPayload, getPaymentMasterAvroPayload}
 
@@ -67,19 +62,26 @@ object PaymentsWriter {
       if (dataFormat.equalsIgnoreCase("json")) {
         val nextPayment = Payment.getNextPayment
         val paymentSummaryRecord = getPaymentJsonPayload(nextPayment)
-        val paymentMasterRecord = getPaymentMasterJsonPayload(nextPayment)
-        val invoiceRecord: InvoiceJson = paymentMasterRecord.getInvoice
 
-        val invoiceProducerRecord = new ProducerRecord[String, InvoiceJson](conf.getString(INVOICES_TOPIC),invoiceRecord.getInvoiceNumber, invoiceRecord)
+        val invoiceRecord: InvoiceJson = InvoiceJsonPayload.getInvoice(nextPayment.invoice)
+        invoiceRecord.setCustomerId(nextPayment.customer.customerId)
+        invoiceRecord.setStoreId(nextPayment.store.storeId)
+
+        val invoiceProducerRecord = new ProducerRecord[String, InvoiceJson](conf.getString(INVOICES_TOPIC), invoiceRecord.getInvoiceNumber, invoiceRecord)
         invoicesJsonProducer.send(invoiceProducerRecord, new AsynchronousProducerCallback)
 
-        val paymentsProducerRecord = new ProducerRecord[String, PaymentJson](conf.getString(PAYMENTS_TOPIC),paymentSummaryRecord.getPaymentId, paymentSummaryRecord)
+        val paymentsProducerRecord = new ProducerRecord[String, PaymentJson](conf.getString(PAYMENTS_TOPIC), paymentSummaryRecord.getPaymentId, paymentSummaryRecord)
         paymentsJsonProducer.send(paymentsProducerRecord, new AsynchronousProducerCallback)
       } else {
         val nextPayment = Payment.getNextPayment
         val paymentSummaryRecord = getPaymentAvroPayload(nextPayment)
-        val paymentMasterRecord = getPaymentMasterAvroPayload(nextPayment)
-        val invoiceRecord: InvoiceAvro = paymentMasterRecord.getInvoice
+
+        val invoiceRecord: InvoiceAvro = {
+          InvoiceAvroPayload.getInvoiceAvroBuilder(nextPayment.invoice)
+            .setCustomerId(nextPayment.customer.customerId)
+            .setStoreId(nextPayment.store.storeId)
+            .build()
+        }
 
         val invoiceProducerRecord = new ProducerRecord[String, InvoiceAvro](conf.getString(INVOICES_TOPIC), invoiceRecord.getInvoiceNumber, invoiceRecord)
         invoicesAvroProducer.send(invoiceProducerRecord, new AsynchronousProducerCallback)
